1d98e8deddc4b76ec444ccac805713e42aef6ba2
alessandrobarreto <alessandrobarreto1201@gmail.com>
2022-01-20 16:49:28 +0000

    feature: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
 import "./styles/App.scss";
 
 function App() {
@@ -10,7 +17,19 @@ function App() {
       <div className="Navbar">
         <Navbar />
       </div>
-      <div className="Main"></div>
+      <div className="Main">
+        <Layout>
+          <div className="route">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
       <div className="Footer"></div>
     </div>
   );
