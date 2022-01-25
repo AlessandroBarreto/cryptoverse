a8fe37b02d2c2d0a0c4fc4c615035db9c9a1cba3
alessandrobarreto <alessandrobarreto1201@gmail.com>
2022-01-25 17:59:28 +0000

    fix: homepage global stats grid

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -21,20 +21,28 @@ export const Homepage = () => {
         Global Crypto Stats
       </Title>
       <Row>
-        <Col span={6}>
+        <Col span={12}>
           <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
           <Statistic
             title="Total Exchanges"
             value={millify(globalStats.totalExchanges)}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
             title="Total Market Cap"
             value={millify(globalStats.totalMarketCap)}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
             title="Total 24h Volume"
             value={millify(globalStats.total24hVolume)}
           />
+        </Col>
+        <Col span={12}>
           <Statistic
             title="Total Markets"
             value={millify(globalStats.totalMarkets)}
